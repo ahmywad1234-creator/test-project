d1f32d3528903d32e7fefc29a1e011bd9530fb66
Ahmed Wadeaa <ahmywad1234@gmail.com>
2025-12-15 11:49:32 +0200

    updata project

--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -6,22 +6,43 @@ export default function Portfolio() {
       <head>
         <title>protfolio</title>
       </head>
-      <section className=' p-5'>
+      <section className='py-5'>
         <div className="container">
-          <h2 className="text-center fw-bold fs-1 mt-5 pt-5">portfolio component</h2>
-          <div className="d-flex justify-content-center align-items-center my-4">
-            <div className="bg-dark" style={{ width: 80, height: 4 }} />
-            <i className="fa-solid fa-star mx-3" />
-            <div className="bg-dark" style={{ width: 80, height: 4 }} />
+          <div className="title my-3">
+            <h2 className="text-center fw-bold fs-1 mt-5 pt-5">portfolio component</h2>
+            <div className="d-flex justify-content-center align-items-center">
+              <div className="bg-dark" style={{ width: 80, height: 4 }} />
+              <i className="fa-solid fa-star mx-3" />
+              <div className="bg-dark" style={{ width: 80, height: 4 }} />
+            </div>
           </div>
+
           <div className="row g-5">
-            <img className='col-lg-4 col-md-6' src="https://routeegy.github.io/startFramework/assets/images/poert1.png" alt="" />
-            <img className='col-lg-4 col-md-6' src="https://routeegy.github.io/startFramework/assets/images/port2.png" alt="" />
-            <img className='col-lg-4 col-md-6' src="https://routeegy.github.io/startFramework/assets/images/port3.png" alt="" />
-            <img className='col-lg-4 col-md-6' src="https://routeegy.github.io/startFramework/assets/images/poert1.png" alt="" />
-            <img className='col-lg-4 col-md-6' src="https://routeegy.github.io/startFramework/assets/images/port2.png" alt="" />
-            <img className='col-lg-4 col-md-6' src="https://routeegy.github.io/startFramework/assets/images/port3.png" alt="" />
+            <div className="col-md-6 col-lg-4">
+              <img className='w-100' src="https://routeegy.github.io/startFramework/assets/images/poert1.png" alt="" />
+            </div>
+
+            <div className="col-md-6 col-lg-4">
+              <img className='w-100' src="https://routeegy.github.io/startFramework/assets/images/port2.png" alt="" />
+            </div>
+
+            <div className="col-md-6 col-lg-4">
+              <img className='w-100' src="https://routeegy.github.io/startFramework/assets/images/port3.png" alt="" />
+            </div>
+
+            <div className="col-md-6 col-lg-4">
+              <img className='w-100' src="https://routeegy.github.io/startFramework/assets/images/poert1.png" alt="" />
+            </div>
+
+            <div className="col-md-6 col-lg-4">
+              <img className='w-100' src="https://routeegy.github.io/startFramework/assets/images/port2.png" alt="" />
+            </div>
+
+            <div className="col-md-6 col-lg-4">
+              <img className='w-100' src="https://routeegy.github.io/startFramework/assets/images/port3.png" alt="" />
+            </div>
           </div>
+
         </div>
       </section>
     </>
